39c4fdc22eba054ecaedd74b5b9f881e247335c5
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-02 21:29:13 -0500

    Refactors the 'App.js' file to show the total number of Kanye quotes available

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import { observer, inject } from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 import JSONPretty from 'react-json-pretty';
 
 import './App.css';
 
 class App extends Component {
-	componentDidMount() {
-		this.props.QuoteStore.loadQuotes();
-	}
+	// componentDidMount() {
+	// 	this.props.QuoteStore.loadQuotes();
+	// }
 
 	render() {
+		const { QuoteStore } = this.props;
+
 		return (
 			<div className='App'>
 				<h1>Welcome to the Kanye Random Quotes Generator!</h1>
+				<h2>Total Number of Kanye Quotes: {QuoteStore.quoteCount} </h2>
 				<JSONPretty json={this.props.QuoteStore.quoteData} />
 			</div>
 		);
