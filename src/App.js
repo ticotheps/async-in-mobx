0e5ffdad8980a544d05133408f931b8c9657aeeb
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-02 20:59:25 -0500

    Renames the 'QuotesStore.js' file to 'QuoteStore.js'

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ import './App.css';
 
 class App extends Component {
 	componentDidMount() {
-		this.props.QuotesStore.loadQuotes();
+		this.props.QuoteStore.loadQuotes();
 	}
 
 	render() {
 		return (
 			<div className='App'>
 				<h1>Welcome to the Kanye Random Quotes Generator!</h1>
-				<JSONPretty json={this.props.QuotesStore.quotesData} />
+				<JSONPretty json={this.props.QuoteStore.quoteData} />
 			</div>
 		);
 	}
 }
 
-export default inject('QuotesStore')(observer(App));
+export default inject('QuoteStore')(observer(App));
