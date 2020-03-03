a096ec18ea0c912a729e07cad694908f9bc4fcc9
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-02 20:52:15 -0500

    Refactors the <App /> component to include the use of the JSONPretty module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
+import JSONPretty from 'react-json-pretty';
+
 import './App.css';
 
 class App extends Component {
+	componentDidMount() {
+		this.props.QuotesStore.loadQuotes();
+	}
+
 	render() {
 		return (
 			<div className='App'>
 				<h1>Welcome to the Kanye Random Quotes Generator!</h1>
+				<JSONPretty json={this.props.QuotesStore.quotesData} />
 			</div>
 		);
 	}
