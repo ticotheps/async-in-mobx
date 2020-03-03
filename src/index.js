6b278c62049f40c978419ff9e670016ad7cf3688
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-02 21:07:19 -0500

    Imports the 'Provider' module (from the 'mobx-react' package) into the 'index.js' file; creates a 'Root' variable to wrap the <App /> component with the MobX <Provider> tags

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'mobx-react';
+
+const Root = (
+	<Provider>
+		<App />
+	</Provider>
+);
+
+ReactDOM.render(Root, document.getElementById('root'));
